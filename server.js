18a0a422b89ddc58e06d3fd4cd626d4de6d93783
Samuel Kleine <kle20001@byui.edu>
2022-03-15 17:05:03 +0000

    new server file and npm express

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req,res) => {res.send("Hello Browser")});
+
+app.listen(port, ()=>console.log("Listening"));
