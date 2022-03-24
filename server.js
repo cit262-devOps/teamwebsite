e2409b2531b6e8d0cdce2de711d504f29743db3a
Samuel-Kleine <97302079+Samuel-Kleine@users.noreply.github.com>
2022-03-24 11:25:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const app = express();
 app.get('/', (req,res) => {res.send("Hello Browser")});
 app.get('/travis', (req,res) => {res.send("Hello Travis")});
 app.get('/gary', (req,res) => {res.send("Hello Gary")});
-
-
-app.get('/', (req,res) => {res.send("Hello Sam")});
+app.get('sam', (req,res) => {res.send("Hello Sam")});
 
 app.listen(port, ()=>console.log("Listening"));
