df7490dba8e467c9978726dd76450bda396ee782
Samuel Kleine <kle20001@byui.edu>
2022-03-24 17:08:50 +0000

    new folders and files for web pod export

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+import express from "express";
 
 const port = 3000;
 
 const app = express();
 
 app.get('/', (req,res) => {res.send("Hello Browser")});
+
+app.get('/', (req,res) => {res.send("Hello Sam")});
 
 app.listen(port, ()=>console.log("Listening"));
---
+++ test/loadsam.test.js
@@ -0,0 +1,10 @@
+import loadsamurl from '..utils/loadsamurl.mjs';
+import assert from 'assert';
+
+it("Tests Sam URL endpoint",async ()=>{
+
+    const urlResponse = await loadsamurl();
+    console.log("URL Response: ")+urlResponse;
+    assert.equal(urlResponse, "Hello Sam");
+
+});
