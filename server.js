92d950212ba36dfa03cca7471d94c1019befbd5b
flint64 <twda96@gmail.com>
2022-03-29 11:07:38 -0600

    server.js changes

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.get('/', (req,res) => {res.send("Hello Browser")});
 app.get('/travis', (req,res) => {res.send("Hello Travis")});
 app.get('/gary', (req,res) => {res.send("Hello Gary")});
 app.get('/sam', (req,res) => {res.send("Hello Sam")});
+app.get('/conner', (req,res) => {res.send("Hello conner")});
 
 app.listen(port, ()=>console.log("Listening"));
