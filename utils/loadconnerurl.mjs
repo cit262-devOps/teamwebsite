e95c4511185632fe391c88b2c4fdd2c21dfcfdf0
conwads <Wadsworthjc97@gmail.com>
2022-03-29 10:55:53 -0600

    made some changes

---
+++ utils/loadconnerurl.mjs
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+
+const loadconnerurl = async ()=>{
+    
+    const options = {
+    method: "GET",
+    headers: {"Content-Type":"application/json"}
+    };
+
+    const apiResponse = await fetch("http://34.72.62.27/conner" , options);
+    const testResponse = await apiResponse.text();
+    return testResponse;
+};
+
+export default loadconnerurl;
